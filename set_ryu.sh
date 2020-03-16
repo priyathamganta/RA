17f99e940a0bc8050b1d9cfe8671f8830085afa1
priyathamganta <47489106+priyathamganta@users.noreply.github.com>
2020-03-15 19:47:30 -0700

    Delete set_ryu.sh

--- set_ryu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-apt-get update
-git clone https://github.com/osrg/ryu.git
-apt install gcc python-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev zlib1g-dev -y
-apt install python-pip
-cd ryu
-pip install -r tools/pip-requires
-python setup.py install
