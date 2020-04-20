1ed95718584bad764faed92899fcf66fabc8bd02
priyathamganta <47489106+priyathamganta@users.noreply.github.com>
2020-04-19 17:42:07 -0700

    Create set_floodlight.sh

---
+++ set_floodlight.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+apt-get update
+apt-get install default-jdk -y 
+apt-get install default-jre -y 
+apt-get install build-essential ant maven python-dev -y
+
+git clone git://github.com/floodlight/floodlight.git -b v1.2
+cd floodlight 
+git submodule update 
+ant 
+mkdir /var/lib/floodlight 
+chmod 777 /var/lib/floodlight
