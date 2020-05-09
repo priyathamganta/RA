f9a8a53540647c1c2263fb6b6c9157f0ef572424
Priyatham Ganta <47489106+priyathamganta@users.noreply.github.com>
2020-05-09 14:54:58 -0700

    Update set_ryu.sh

--- set_ryu.sh
+++ set_ryu.sh
@@ -3,7 +3,8 @@
 sudo apt-get update
 sudo git clone https://github.com/osrg/ryu.git
 sudo apt install gcc python-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev zlib1g-dev -y
-sudo apt-get install python-pip -y
+sudo apt-get install python3-pip -y
 cd ryu/
-pip install -r tools/pip-requires
-sudo python setup.py install
+pip3 install -r tools/pip-requires
+pip3-install -r tools/optional-requires
+sudo python3 setup.py install
